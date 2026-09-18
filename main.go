427b880895ee0b2c8df2089a1591b43f449c56d0
AlladinDev <bhatsaqlain982@gmail.com>
2026-09-18 18:15:21 +0530

    Fixed issues with gateway

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"net"
 	"net/http"
@@ -18,21 +20,19 @@ var ipRegistry sync.Map
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080" // Local fallback
+		port = "8000" // Local fallback
 	}
 	proxy := &httputil.ReverseProxy{
 		Director: func(r *http.Request) {
 			//first get the ip address of the user
-			ipAddr := ""
-
-			//now maybe the user is behind load balancer so extract the real ip
-			// If behind a proxy/load balancer, prioritize the real client IP [1]
-			if forwarded := r.Header.Get("X-Forwarded-For"); forwarded != "" {
-				ipAddr = forwarded
+			ipAddrAny := r.Context().Value("userIP")
+			ipAddress, ok := ipAddrAny.(string)
+			if !ok {
+				fmt.Println("failed to parse ip address got from ctx into string format")
 			}
 
 			//now we have to check if this ipAddr is present in ip registery or not i mean whether it is registered first or not
-			if val, exists := ipRegistry.Load(ipAddr); exists {
+			if val, exists := ipRegistry.Load(ipAddress); exists {
 
 				redirectUrl, ok := val.(*url.URL)
 				if ok {
@@ -57,9 +57,20 @@ func main() {
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		targetUrl := r.URL.Query().Get("url")
-		ipAddr, _, err := net.SplitHostPort(r.RemoteAddr)
-		if err != nil {
-			ipAddr = r.RemoteAddr
+		ipAddr := ""
+		if forwarded := r.Header.Get("X-Forwarded-For"); forwarded != "" {
+			ipAddr = forwarded
+		}
+
+		if ipAddr == "" {
+			//if not this header then get the ip address from req object
+			ip, _, err := net.SplitHostPort(r.RemoteAddr)
+			if err != nil {
+				w.Write([]byte(err.Error()))
+				return
+			}
+
+			ipAddr = ip
 		}
 		if targetUrl != "" {
 			//if port is not empty it means user wants to register endpoint
@@ -83,9 +94,31 @@ func main() {
 			return
 		}
 
-		proxy.ServeHTTP(w, r)
+		updatedCtx := context.WithValue(r.Context(), "userIP", ipAddr)
+
+		proxy.ServeHTTP(w, r.WithContext(updatedCtx))
 	})
 
+	mux.HandleFunc("/devices", func(w http.ResponseWriter, r *http.Request) {
+		ipsConnected := []any{}
+		ipRegistry.Range(func(key, value any) bool {
+			ipsConnected = append(ipsConnected, key)
+			return true
+		})
+
+		resp, err := json.Marshal(map[string]any{
+			"Message": "Saved Ips",
+			"IPS":     ipsConnected,
+		})
+
+		if err != nil {
+			fmt.Print(err.Error())
+			w.Write([]byte("failed to get devices connected"))
+			return
+		}
+
+		w.Write(resp)
+	})
 	fmt.Printf("Server started successfully")
 	if err := http.ListenAndServe("0.0.0.0:"+port, CorsMiddleware(mux)); err != nil {
 		fmt.Println("failed to start server err is : ", err)
